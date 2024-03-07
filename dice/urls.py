58950170a2fb690de81beac62bac552d7057351e
Ilippy <Ilippyk@gmail.com>
2024-03-07 21:08:42 +0300

    added models for coin

--- dice/urls.py
+++ dice/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('dice/', views.dice),
-    path('coin', views.coin),
-    path('hundred', views.hundred),
+    path('coin/<int:amount_flips>', views.coin),
+    path('hundred/', views.hundred),
 ]
--- dice/views.py
+++ dice/views.py
@@ -2,20 +2,31 @@ from django.shortcuts import render
 from random import randint, choice
 from django.http import HttpResponse
 import logging
+from .models import CoinFlip
 
 logger = logging.getLogger(__name__)
 
+
 # Create your views here.
+
+
 def dice(request):
     result = randint(1, 6)
     logger.info(result)
     return HttpResponse(result)
 
 
-def coin(request):
+def coin(request, amount_flips):
     result = choice(('Head', 'Tails'))
     logger.info(result)
-    return HttpResponse(result)
+    CoinFlip(side=result).save()
+    last_results = CoinFlip.get_last_flips(amount_flips)
+    context = {
+        'current_flip': result,
+        'last_results': last_results
+    }
+    return render(request, 'dice/coin.html', context)
+    # return HttpResponse(result)
 
 
 def hundred(request):
