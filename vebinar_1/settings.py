837dd0f3324590f7ca90909fae748d4a65f3ba18
Ilippy <Ilippyk@gmail.com>
2024-03-29 16:01:25 +0300

    fixed bug in settings.py

--- vebinar_1/settings.py
+++ vebinar_1/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
         'HOST': 'Ilippy.mysql.pythonanywhere-services.com',
         'OPTIONS': {
-            'init_commands': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4'
         }
     }
